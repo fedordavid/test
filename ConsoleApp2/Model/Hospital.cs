584110850ec974d62a90db318be5b5b034b06aa0
David Fedor <david.fedor@fifa.org>
2022-10-03 21:26:34 +0200

    Change Add Gift

--- ConsoleApp2/Model/Hospital.cs
+++ ConsoleApp2/Model/Hospital.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ausing System;
 using System.Security.Cryptography;
 
 namespace ConsoleApp2.Model
@@ -9,5 +9,6 @@ namespace ConsoleApp2.Model
         public string HospitalName { get; set; }
         
         public string Address { get; set; }
+        public string Gift { get; set; }
     }
 }
